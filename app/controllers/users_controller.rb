51a4f4c2a697b0c61cf2484791237950cb7d0c5c
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-05 18:21:26 +0000

    strong prameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
   end
 end
