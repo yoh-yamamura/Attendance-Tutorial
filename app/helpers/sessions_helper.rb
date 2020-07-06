649dc5adbfac115d2c556c7c0517dac3f8b9fb93
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-06 22:43:30 +0000

    6. 3. 2 current_userを定義しよう

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+  
+  
+   # 引数に渡されたユーザーオブジェクトでログインします。
+  def log_in
+    sessions[:user_id] = user.id
+  end  
 end
+
+   # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+   def current user
+     if session[:user_id]
+       @current user ||= User.find_by(id:session[:user_id])
+     end
+   end   
