1e92eb669e25d3af3d3134cbd10d5301b853d1d0
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-06 11:03:07 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規登録に成功しました。'
+      redirect_to @user
+      # 保存に成功した場合は、ここに記述した処理が実行されます。
+    else
+      render :new
+    end
+  end  
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
