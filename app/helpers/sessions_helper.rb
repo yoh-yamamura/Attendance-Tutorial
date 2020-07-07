9c44f9007e2d83bc271eba089aec6b3c64754eb8
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-07 11:29:46 +0000

    Finish login-function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,14 +2,29 @@ module SessionsHelper
   
   
    # 引数に渡されたユーザーオブジェクトでログインします。
-  def log_in
-    sessions[:user_id] = user.id
+  def log_in(user)
+    session[:user_id] = user.id
   end  
-end
+  
+  # セッションと@current_userを破棄します
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end  
+
 
    # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
-   def current user
+   def current_user
      if session[:user_id]
-       @current user ||= User.find_by(id:session[:user_id])
+       @current_user ||= User.find_by(id:session[:user_id])
      end
    end   
+
+
+   
+   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
+   def logged_in?
+     !current_user.nil?
+   end
+end   
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
-  
+
   # ログイン機能
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
