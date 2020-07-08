4af23fd9d180493cdf55c4a7627e99900764f997
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-08 14:35:47 +0000

    8. 1. 2 更新に失敗した場合の処理

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,32 +1,31 @@
 module SessionsHelper
-  
-  
-   # 引数に渡されたユーザーオブジェクトでログインします。
+
+  # 引数に渡されたユーザーオブジェクトでログインします。
   def log_in(user)
     session[:user_id] = user.id
-  end  
-  
+  end
+
   # 永続的セッションを記憶します（Userモデルを参照）
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
-  end  
-  
+  end
+
   # 永続的セッションを破棄します
   def forget(user)
     user.forget # Userモデル参照
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
-  end  
-  
+  end
+
   # セッションと@current_userを破棄します
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
-  end  
-  
+  end
+
   # 一時的セッションにいるユーザーを返します。
   # それ以外の場合はcookiesに対応するユーザーを返します。
   def current_user
@@ -40,21 +39,9 @@ module SessionsHelper
       end
     end
   end
-      
 
-
-  # # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
-  # def current_user
-  #   if session[:user_id]
-  #     @current_user ||= User.find_by(id:session[:user_id])
-  #   end
-  # end   
-
-
-   
-   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
-   def logged_in?
-     !current_user.nil?
-   end
-end   
-
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
+  def logged_in?
+    !current_user.nil?
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -45,6 +45,6 @@ class User < ApplicationRecord
 
   # ユーザーのログイン情報を破棄します。
   def forget
-    update_attribute(:remember_digest)
+    update_attribute(:remember_digest,nil)
   end  
 end  
