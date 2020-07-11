143b13e91a90954a95b8567e39002481c0bdf2f1
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-11 03:40:02 +0000

    8. 5. 2 destroyアクション

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
      flash.now[:danger] = "認証に失敗しました。"
+     redirect_back_or user
      render :new
     end
   end  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end  
+  
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -21,21 +28,42 @@ class UsersController < ApplicationController
   end  
   
   def edit
-    @user = User.find(params[:id])
   end  
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-    # 更新に成功した場合の処理を記述します。
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
     else
-      render :edit
-    end  
-  end    
-  
-  private
+      render :edit      
+    end
+  end  
 
+  private
+  
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    # beforeフィルター
+    
+    # paramsハッシュからユーザーを取得します。
+    
+    def set_user
+      @user = User.find(params[:id])
+    end 
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in? 
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end  
+    
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    
+    def correct_user
+      redirect_to(root_url) unless  current_user?(@user)
+    end  
 end
