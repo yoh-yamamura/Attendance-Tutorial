fa0849951cade20f93a5e1ed9209538d1f86982d
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-12 16:38:48 +0000

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,13 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
-  
+  before_action :set_one_month, only: [:show]
   def index
     @users = User.paginate(page: params[:page])
   end  
   
   def show
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
   
   def new
@@ -70,7 +69,7 @@ class UsersController < ApplicationController
     # beforeフィルター
     
     def basic_info_params
-      params.require(:user).permit(:department, :work_time, :basic_time)
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
     # paramsハッシュからユーザーを取得します。
     
